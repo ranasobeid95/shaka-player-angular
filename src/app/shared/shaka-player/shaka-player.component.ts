c11ea214b0e818014320c085d611be000ee79109
ranasobeid95 <ranasobeid95@gmail.com>
2022-02-06 11:57:59 +0200

    Initialize the player

---
+++ src/app/shared/shaka-player/shaka-player.component.ts
@@ -0,0 +1,53 @@
+import { Component,OnInit, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+
+declare let shaka:any ;
+@Component({
+  selector: 'app-shaka-player',
+  templateUrl: './shaka-player.component.html',
+  styleUrls: ['./shaka-player.component.css']
+})
+export class ShakaPlayerComponent implements OnInit {
+  @ViewChild('videoPlayer') videoElementRef: ElementRef | undefined;
+  @ViewChild('videoContainer') videoContainerRef: ElementRef | undefined;
+
+  videoElement: HTMLVideoElement | undefined;
+  videoContainerElement: HTMLDivElement | undefined;
+  player: any;
+
+  constructor() { }
+
+  private initPlayer() {
+    this.player = new shaka.Player(this.videoElement);
+  
+    const ui = new shaka.ui.Overlay(
+      this.player,
+      this.videoContainerElement,
+      this.videoElement
+    );
+  
+    this.player
+      .load('http://www.bok.net/dash/tears_of_steel/cleartext/stream.mpd')
+      .then(() => {
+        this.videoElement?.play();
+      })
+      .catch((e: any) => {
+        console.error(e);
+      });
+  }
+
+
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    shaka.polyfill.installAll();
+    if (shaka.Player.isBrowserSupported()) {
+      this.videoElement = this.videoElementRef?.nativeElement;
+      this.videoContainerElement = this.videoContainerRef?.nativeElement;
+      this.initPlayer();
+    } else {
+      console.error('Browser not supported!');
+    }
+  }
+
+}
